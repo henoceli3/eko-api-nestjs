68b02fe7ec5f954a9542d4755f5ffc615d9c9a20
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2023-11-02 19:40:40 +0000

    3

--- database.config.ts
+++ database.config.ts
@@ -1,12 +1,13 @@
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export const bdConfig: MysqlConnectionOptions = {
   type: 'mariadb',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '',
-  database: 'eko-api-nestjs',
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: true,
 };
