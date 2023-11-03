4eaffc68c55fb575408127df1fd7c9a28ab8e76c
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2023-11-03 09:06:13 +0000

    5

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,19 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { WalletsModule } from './wallets/wallets.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { bdConfig } from 'database.config';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const bdConfig: MysqlConnectionOptions = {
+  type: 'mariadb',
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
 
 @Module({
   imports: [TypeOrmModule.forRoot(bdConfig), UsersModule, WalletsModule],
